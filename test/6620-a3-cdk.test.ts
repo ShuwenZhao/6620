38521e0ea3c3afaafc26f77972773c788b4d123b
Shuwen Zhao <shuwenzhao@yahoo.com>
2024-11-01 00:23:42 +0000

    Initial commit of cdk project

---
+++ test/6620-a3-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as 6620A3Cdk from '../lib/6620-a3-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/6620-a3-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new 6620A3Cdk.6620A3CdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
